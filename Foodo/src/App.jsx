33424ffc7ccbcef280ffec7e5d085536688866a7
Wish245 <sankhawishwanath08@gmail.com>
2024-08-27 17:15:08 +0530

    chore: Add initial project files and dependencies

---
+++ Foodo/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavBar from './pages/NavBar/NavBar'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <NavBar/>
+    </div>
+  )
+}
+
+export default App
