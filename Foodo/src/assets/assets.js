42f61351d7085a2fee28ef34b985a313c4d7031d
Wish245 <sankhawishwanath08@gmail.com>
2024-08-28 06:02:30 +0530

    chore: Update CSS styles for logo, navbar, and root container

--- Foodo/src/assets/assets.js
+++ Foodo/src/assets/assets.js
@@ -1,1 +1,21 @@
-import 
+import Muffins from './Muffins.jpg'
+import paellaPhaidon from './paella-phaidon.jpg'
+import pizza from './pizza.jpg'
+import curd from './curd.jpg'
+import burger from './burger.jpg'
+import header from './header.jpg'
+import search from './search.png'
+import shoppingBasket from './shopping-basket.png'
+import logo from './logo.png'
+
+export const assets = {
+    Muffins,
+    paellaPhaidon,
+    pizza,
+    curd,
+    burger,
+    header,
+    search,
+    logo,
+    shoppingBasket
+}
